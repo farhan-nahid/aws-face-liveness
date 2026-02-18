da41263c251f41f12f18ae200fe12b2892ca6765
Farhan <nahidahmed9933@gmail.com>
2026-02-19 05:38:29 +0600

    fix: Correct API endpoint variable name for face recognition service

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 export const API_ENDPOINTS = {
-  CREATE_LIVENESS_SESSION: `${import.meta.env.VITE_FACE_RECEPTION_API_URL}/liveness`,
-  CREATE_TEMPORARY_CREDENTIALS: `${import.meta.env.VITE_FACE_RECEPTION_API_URL}/liveness/temporary-credentials`,
-  GET_RESULT: (sessionId: string, threshold: number) => `${import.meta.env.VITE_FACE_RECEPTION_API_URL}/liveness/${sessionId}/${import.meta.env.VITE_COMPANY_ID}?threshold=${threshold}`,
+  CREATE_LIVENESS_SESSION: `${import.meta.env.VITE_FACE_REC_API_URL}/liveness`,
+  CREATE_TEMPORARY_CREDENTIALS: `${import.meta.env.VITE_FACE_REC_API_URL}/liveness/temporary-credentials`,
+  GET_RESULT: (sessionId: string, threshold: number) => `${import.meta.env.VITE_FACE_REC_API_URL}/liveness/${sessionId}/${import.meta.env.VITE_COMPANY_ID}?threshold=${threshold}`,
   GET_USER_INFO: (userId: string) => `${import.meta.env.VITE_HR_API_URL}/employee/single-employee/${userId}/`,
 };
